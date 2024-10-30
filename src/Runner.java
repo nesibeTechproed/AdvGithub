076632d08164bf0caede5746de708dba2e040a77
nesibeTechproed <129404713+nesibeTechproed@users.noreply.github.com>
2024-10-30 13:39:55 +0300

    dev2

--- src/Runner.java
+++ src/Runner.java
@@ -26,8 +26,8 @@ public class Runner {
 
         //localde değişiklik
 
-        //remote repoda değşiklik
-        //---------------------------
+        //dev2 nin yaptığı değşiklik
+        //........................
 
 
     }
